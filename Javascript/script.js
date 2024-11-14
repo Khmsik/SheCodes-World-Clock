69e43e71d331e755a9c321fcf918bd083ef83133
Khmsik <sthabilekhumalo99@gmail.com>
2024-11-14 11:51:54 +0200

    added 3rd city functionality

--- Javascript/script.js
+++ Javascript/script.js
@@ -23,6 +23,18 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  //Johannesburg
+  let johannesburgElement = document.querySelector("#Johannesburg");
+  if (sydneyElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
