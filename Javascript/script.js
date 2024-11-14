923b241b9b1e8e2a41d2db07f34e38ff1cafb4a6
Khmsik <sthabilekhumalo99@gmail.com>
2024-11-14 10:36:59 +0200

    added the update time function

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+//tokyo
+function updateTime() {
+  let tokyoElement = document.querySelector("#Tokyo");
+
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Sydney
+  let sydneyElement = document.querySelector("#Sydney");
+
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
